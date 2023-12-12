f8e1ef7ec9ddd178e8354e5474bd0b042994951b
Alain2020 <favouralain@yahoo.com>
2023-12-12 00:15:18 +0000

    Modify script.js to display greetings in the browser
    Update the script.js file to display greetings in the browser in addition to printing to the console. If the name starts with a letter 'J' or 'j', it will print 'Goodbye JSomeName', otherwise 'Hello SomeName'. Update the HTML file to include the JavaScript file.

--- script.js
+++ script.js
@@ -7,13 +7,22 @@
     for (var i = 0; i < names.length; i++) {
       // Your conditional statements go here
       var name = names[i];
+      var greetingMessage;
   
       // Check if the name starts with 'J' or 'j'
       if (name.charAt(0).toLowerCase() === 'j') {
-        console.log("Goodbye " + name);
+        greetingMessage = "Goodbye " + name;
       } else {
-        console.log("Hello " + name);
+        greetingMessage = "Hello " + name;
       }
+  
+      // Log to the console
+      console.log(greetingMessage);
+  
+      // Display in the browser
+      var messageElement = document.createElement("p");
+      messageElement.textContent = greetingMessage;
+      document.body.appendChild(messageElement);
     }
   })();
   
